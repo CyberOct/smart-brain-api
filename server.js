decc52c22bb71900adb1069179d8d489dfa38a98
Stefan Butura <stefan.butura@gmail.com>
2020-08-18 23:17:57 +0300

    more DB url issues solved

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const image = require('./controlers/image');
 var db = require('knex')({
   client: 'pg',
   connection: {
-   host: process.env.DATABASE_URL,
+   connectionString: process.env.DATABASE_URL,
    ssl:true
   }
 });
